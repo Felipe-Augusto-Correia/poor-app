c56d9ec898a7f2011efbbfbbfa3166e7b21ad471
Felipe Augusto <51971892+Felipe-Augusto-Correia@users.noreply.github.com>
2021-05-05 11:55:51 -0300

    debug banner false

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.blueGrey[500],
         appBar: AppBar(
